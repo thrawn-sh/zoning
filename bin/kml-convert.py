8bb2d9a247772a058e3a7a84dcccaa6a9fc822bc
Alexander Dreweke <alexander@dreweke.net>
2019-07-19 08:52:57 +0200

    added management csv

--- bin/kml-convert.py
+++ bin/kml-convert.py
@@ -79,7 +79,7 @@ def kmz_to_geojson(kml, output_folder='api'):
         with open(output_folder + '/' + postal_code + '.geojson', 'w') as outfile:
             json.dump(feature, outfile, indent=4, ensure_ascii=False)
 
-def kmz_to_json(kml, city_dictionary, state_dictionary, population_dictionary, output_folder='api'):
+def kmz_to_json(kml, city_dictionary, state_dictionary, population_dictionary, management_dictionary, output_folder='api'):
     coordinates = { }
 
     for pm in kml.cssselect('Placemark'):
@@ -98,18 +98,17 @@ def kmz_to_json(kml, city_dictionary, state_dictionary, population_dictionary, o
         points = coordinates[postal_code]
 
         center = calculate_center(points)
-        # external data
-        license_partner = None
-        place = city_dictionary[postal_code]
-        population = population_dictionary[postal_code]
-        state = state_dictionary[postal_code]
+        manager = management_dictionary.get(postal_code)
+        place = city_dictionary.get(postal_code)
+        population = population_dictionary.get(postal_code)
+        state = state_dictionary.get(postal_code)
 
         element = collections.OrderedDict()
-        element['center']           = center
-        element['license_partner']  = license_partner
-        element['place']            = place
-        element['population']       = population
-        element['state']            = state
+        element['center']     = center
+        element['manager']    = manager
+        element['place']      = place
+        element['population'] = population
+        element['state']      = state
 
         with open(output_folder + '/' + postal_code + '.json', 'w') as outfile:
             json.dump(element, outfile, indent=4, ensure_ascii=False)
@@ -117,7 +116,8 @@ def kmz_to_json(kml, city_dictionary, state_dictionary, population_dictionary, o
 def main():
     parser = argparse.ArgumentParser(description="generate api data", add_help=True)
     parser.add_argument("-a", "--area",       help="area CSV",       required=True, default='area.csv'      )
-    parser.add_argument("-o", "--output",     help="output folder",  required=True, default='./api'         )
+    parser.add_argument("-m", "--management", help="management CSV", required=True, default='management.csv')
+    parser.add_argument("-o", "--output",     help="output folder",  required=True, default='api'           )
     parser.add_argument("-p", "--population", help="population CSV", required=True, default='population.csv')
     parser.add_argument("-z", "--zone",       help="zone KMZ",       required=True, default='zone.kmz'      )
 
@@ -143,10 +143,17 @@ def main():
             postal_code = row['plz']
             population[postal_code] = row['einwohner']
 
+    management = { }
+    with open(args.management) as f:
+        dictionary = csv.DictReader(f, delimiter=',')
+        for row in dictionary:
+            postal_code = row['plz']
+            management[postal_code] = row['management']
+
     kml_document = html.fromstring(kml)
-    neighbours = calculate_neighbours(kml_document)
     kmz_to_geojson(kml_document, args.output)
-    kmz_to_json(kml_document, city, state, population, args.output)
+    kmz_to_json(kml_document, city, state, population, management, args.output)
+    #neighbours = calculate_neighbours(kml_document)
 #    kmz_to_neighbors(kml_document, neighbours);
 
 if __name__ == '__main__':
