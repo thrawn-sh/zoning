144b25157408d05e38062b06b3e1d589eef1b55b
Alexander Dreweke <alexander@dreweke.net>
2019-07-22 17:02:35 +0200

    layout cleanup

--- src/application.js
+++ src/application.js
@@ -1,32 +1,15 @@
 "use strict";
 
 var germanyCenter = new L.LatLng(51.0948001, 10.2651007);
+var maxZoom = 14;
 var minZoom = 6;
-
-function setCoordinates(type, lat, lng) {
-    document.getElementById(type + "_lat").value  = lat;
-    document.getElementById(type + "_lng").value = lng;
-}
 
 var map = L.map("map", {
     center:  germanyCenter,
-    maxZoom: 14,
+    maxZoom: maxZoom,
     minZoom: minZoom,
     zoom:    minZoom,
 });
-
-function updateCoordinates() {
-    var bounds = map.getBounds();
-    var center = bounds.getCenter();
-    setCoordinates("center", center.lat,        center.lng      );
-    setCoordinates("north",  bounds.getNorth(), center.lng      );
-    setCoordinates("east",   center.lat,        bounds.getEast());
-    setCoordinates("south",  bounds.getSouth(), center.lng      );
-    setCoordinates("west",   center.lat,        bounds.getWest());
-}
-
-map.on("zoomend",  updateCoordinates);
-map.on("moveend",  updateCoordinates);
 
 var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     "attribution":  "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors",
@@ -34,22 +17,45 @@ var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 });
 map.addLayer(osm);
 
-updateCoordinates();
 
-plz = undefined;
+var plz = undefined;
+var current = undefined;
 
+function onEachFeature(feature, layer) {
+    layer.on('click', function() {
+        var plz = feature.properties.name;
+        if (plz === current) {
+            return;
+        }
+        document.getElementById("search").value = plz;
+        gotoPostalCode();
+    });
+}
+
+var center = undefined;
 function gotoPostalCode() {
     var postalCode = document.getElementById("search").value.trim();
+    if (((!postalCode || 0 === postalCode.length)) || (current === postalCode)) {
+        map.flyTo(center);
+        return
+    }
+    current = postalCode;
 
     var request = new XMLHttpRequest(); // a new request
     request.open("GET", "/api/" + postalCode + ".json", false);
     request.send(null);
     var response = JSON.parse(request.responseText);
 
-    map.flyTo(new L.LatLng(response.center[1], response.center[0]), 13);
+    center = new L.LatLng(response.center[1], response.center[0]);
+    if (true) {
+        map.flyTo(center, (maxZoom - 1));
+    } else {
+        map.flyTo(center);
+    }
 
     if (plz) {
         map.removeLayer(plz);
+        plz = undefined;
     }
 
     var requestGeo = new XMLHttpRequest(); // a new request
@@ -57,6 +63,22 @@ function gotoPostalCode() {
     requestGeo.send(null);
     var responseGeo = JSON.parse(requestGeo.responseText);
 
-    plz = L.geoJSON(responseGeo);
+    plz = L.geoJSON(responseGeo, {
+        style: function(feature) {
+            var request = new XMLHttpRequest(); // a new request
+            var plz = feature.properties.name;
+            request.open("GET", "/api/" + plz + ".json", false);
+            request.send(null);
+            var response = JSON.parse(request.responseText);
+            if (response.manager) {
+                return { fillColor: "#de4f06" };
+            }
+            return { };
+        },
+        onEachFeature: onEachFeature
+    });
+    plz.bindTooltip(function(layer) {
+        return layer.feature.properties.name;
+    }, { sticky: true });
     map.addLayer(plz);
 }
