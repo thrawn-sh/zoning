45a829378838b02a257c1c1b9f3754f1dd94e00f
Alexander Dreweke <alexander@dreweke.net>
2019-07-31 12:58:13 +0200

    rework neighbours calculation

--- bin/calculate_neighbours.py
+++ bin/calculate_neighbours.py
@@ -6,38 +6,50 @@ import csv
 from lxml import html
 import os
 import zipfile
-from shapely.geometry import Polygon
+from shapely.geometry import MultiPoint
 
-def calculate_neighbours(kml):
-    neighbours = { }
+RESOLUTION = 5
+
+def calculate_envelopes(kml):
+    poligons = { }
+    for pm in kml.cssselect('Placemark'):
+        postal_code = pm.cssselect('name')[0].text_content()
+        poligons[postal_code] = [ ]
 
     for pm in kml.cssselect('Placemark'):
         postal_code = pm.cssselect('name')[0].text_content()
+        poligons[postal_code].append(pm.cssselect('coordinates'))
 
+    envelopes = { }
+    for postal_code in poligons:
         points = []
-        for c in pm.cssselect('coordinates'):
-            for p in c.text_content().split(' '):
-                (lng, lat) = p.split(',')
-                points.append((float(lng), float(lat)))
+        for feature_poligons in poligons[postal_code]:
+            for poligon in feature_poligons:
+                for p in poligon.text_content().split(' '):
+                    (lng, lat) = p.split(',')
+                    points.append((round(float(lng), RESOLUTION), round(float(lat), RESOLUTION)))
+        envelopes[postal_code] = MultiPoint(points).envelope
 
-        neighbours[postal_code] = Polygon(points).envelope
+    return envelopes
+
+def calculate_neighbours(kml):
+    envelopes = calculate_envelopes(kml)
 
     result = { }
-    size = len(neighbours)
+    size = len(envelopes)
     count = 0
-    for pm in kml.cssselect('Placemark'):
+    for current in sorted(envelopes):
         count += 1
-        current = pm.cssselect('name')[0].text_content()
         print("%s: [%d/%d]" % (current, count, size))
-        current_polygon = neighbours[current]
-        for neighbour in neighbours:
-            other_polygon = neighbours[neighbour]
+        current_polygon = envelopes[current]
+        for envelop in envelopes:
+            other_polygon = envelopes[envelop]
             if current_polygon.intersects(other_polygon):
                 neighbour_set = set()
                 if current in result:
                     neighbour_set = result[current]
 
-                neighbour_set.add(neighbour)
+                neighbour_set.add(envelop)
                 result[current] = neighbour_set
 
     return result
